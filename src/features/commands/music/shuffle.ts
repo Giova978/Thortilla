8b32e7fa027182c95d37e2661ccd0452a6859662
Giova <giovanni.hawas@gmail.com>
2020-10-21 10:49:29 -0300

    Add shuffle

--- src/features/commands/music/shuffle.ts
+++ src/features/commands/music/shuffle.ts
@@ -27,22 +27,7 @@ module.exports = class extends Command {
         if (musicData.queue.length < 2) return channel.error("Cant shuffle 1 song");
         if (musicData.player.queue.repeatQueue) return channel.error("You cant shuffle while the queue is looping");
 
-        const copy = [...musicData.queue];
-        const playerQueueCopy = musicData.player.queue.KVArray().reduce((acc, val) => {
-            acc.push(val[1]);
-            return acc;
-        }, [] as Track[]);
-        playerQueueCopy.shift();
-        musicData.player.queue.clearQueue();
-
-        const queue = musicData.queue.map((val, index) => {
-            const random = Utils.getRandom(copy.length, 0);
-            // Add one because the index '0' in the player queue is the current song which is not the case with the queue. The queue first index (0) is the next song. So the player queue has a offset of 1
-            musicData.player.queue.add(playerQueueCopy[random]);
-            return copy.splice(random, 1)[0];
-        });
-        musicData.queue = queue;
-
+        this.handler.player.shuffle(message.guild!.id);
         channel.success("Shuffled the queue");
     }
 
